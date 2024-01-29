afe6c59c1cb4dc977e9ae45648e7f0eecf46f95f
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2024-01-30 00:00:03 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,18 +1,16 @@
 local DiscordLib =
     loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
 
-local win = DiscordLib:Window("discord library")
+local win = DiscordLib:Window("Youtube Simulator By - Wreston")
 
 local serv = win:Server("Preview", "")
 
 local btns = serv:Channel("My Restaurant")
 
 btns:Button(
-    "Gold Food",
-    function(customerOwnerUID, customerOwnerID, isRichCustomer, isPirateCustomer, isNearTree)
-    local spoof = Food.new("45", customerOwnerUID, customerOwnerID, true, true)
-    spoof.IsGold = true
-    return spoof
-    end
-
+    "Auto Edit",
+    while true do
+game:GetService("ReplicatedStorage").singleVideo:FireServer(unpack(args))
+    
+end
 )
